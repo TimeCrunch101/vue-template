281f7f3aaf7f3bec6dc80b5c2f64d1926e2661c1
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2022-10-17 12:37:11 -0400

    Initial Template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Hello World"/>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
